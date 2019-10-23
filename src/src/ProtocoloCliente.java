3383911a844488d0d5734681f4da0b18fe41af60
jecanizarez <je.canizarez@uniandes.edu.co>
2019-10-23 10:48:56 -0500

    here we go again

---
+++ src/src/ProtocoloCliente.java
@@ -0,0 +1,14 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ProtocoloCliente {
+	
+	public static void procesar(BufferedReader stdIn, BufferedReader pIn, PrintWriter pOut) throws IOException
+	{
+		
+	}
+
+}
